9005707dea27e737b575481fbd754f75543141bf
Khair Alanam <anaambasheer@gmail.com>
2023-12-11 22:29:17 +0530

    Fix the inheritance

--- classes/YTDownloader.py
+++ classes/YTDownloader.py
@@ -1,6 +1,7 @@
-from Application import Application
+from .Application import Application
 
 
 class YTDownloader(Application):
-    def __init__(self):
+    def __init__(self, name, dimensions):
+        super().__init__(name, dimensions)
         print("YT Initialised!")
