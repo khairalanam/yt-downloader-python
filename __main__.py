a5e467d2109f5714b60c0165d7fadfafa2d80def
Khair Alanam <anaambasheer@gmail.com>
2023-12-11 22:23:40 +0530

    use YTDownloader as the main app

--- __main__.py
+++ __main__.py
@@ -1,5 +1,4 @@
 from classes.Application import Application
 from classes.YTDownloader import YTDownloader
 
-app = Application("YT Downloader", "1024x800")
-yt_downloader = YTDownloader(app)
+app = YTDownloader("YT Downloader", "1024x800")
