56ddf0e55999f6aab63f0daed2ed30069663d1b9
Khair Alanam <anaambasheer@gmail.com>
2023-12-11 23:00:51 +0530

    Modify button and entry methods to includes dimensions and parameters

--- classes/Application.py
+++ classes/Application.py
@@ -8,19 +8,24 @@ class Application:
         self.app = ctk.CTk()
         self.app.title(name)
         self.app.geometry(dimensions)
-        self.app.mainloop()
         print("Initialised!")
 
-    def add_label(self, name, x, y):
-        main_label = ctk.CTkLabel(self.app, text=name)
+    def run_app(self):
+        self.app.mainloop()
+
+    def add_label(self, name, x=20, y=20):
+        main_label = ctk.CTkLabel(
+            self.app, text=name, font=("Helvetica", 20, "bold"))
         main_label.pack(padx=x, pady=y)
 
-    def add_entry(self, text, x, y):
+    def add_entry(self, width, height, x=20, y=20):
         entry_var = ctk.StringVar()
-        entry_box = ctk.CTkEntry(self.app, text=text, textvariable=entry_var)
+        entry_box = ctk.CTkEntry(
+            self.app, textvariable=entry_var, width=width, height=height)
         entry_box.pack(padx=x, pady=y)
         return entry_var
 
-    def add_button(self, name, x, y, callback):
-        main_button = ctk.CTkButton(self.app, text=name, command=callback)
+    def add_button(self, name, width, height, callback, params=None, x=20, y=20):
+        main_button = ctk.CTkButton(
+            self.app, text=name, command=lambda: callback(params), font=("Helvetica", 20, "bold"), width=width, height=height)
         main_button.pack(padx=x, pady=y)
