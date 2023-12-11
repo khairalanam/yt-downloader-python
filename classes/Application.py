430cad400c0f1c403ea59d0831ed1124d12e2c9e
Khair Alanam <anaambasheer@gmail.com>
2023-12-11 21:24:13 +0530

    Add sample app

--- classes/Application.py
+++ classes/Application.py
@@ -1,3 +1,10 @@
+import customtkinter as ctk
+
+
 class Application:
-    def __init__(self):
+    def __init__(self, name, dimensions):
+        app = ctk.CTk()
+        app.title(name)
+        app.geometry(dimensions)
+        app.mainloop()
         print("Initialised!")
