d9cd644ca1d540626e8b09ada2d9dbe59d689c4c
Khair Alanam <anaambasheer@gmail.com>
2023-12-11 23:00:11 +0530

    Add start_download button

--- classes/YTDownloader.py
+++ classes/YTDownloader.py
@@ -5,3 +5,7 @@ class YTDownloader(Application):
     def __init__(self, name, dimensions):
         super().__init__(name, dimensions)
         print("YT Initialised!")
+
+    @staticmethod
+    def start_download(link):
+        print("Started Downloading!")
