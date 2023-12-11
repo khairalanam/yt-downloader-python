1d62bed3928d12531615633d9fe3e6e1550d71f9
Khair Alanam <anaambasheer@gmail.com>
2023-12-11 22:59:47 +0530

    Make a sample working app

--- __main__.py
+++ __main__.py
@@ -1,4 +1,18 @@
-from classes.Application import Application
 from classes.YTDownloader import YTDownloader
+from tkinter import StringVar
 
-app = YTDownloader("YT Downloader", "1024x800")
+# App creation
+app: YTDownloader = YTDownloader("YT Downloader", "1024x800")
+
+# Title
+app.add_label("Insert the playlist link to download", 20, 40)
+
+# Input for playlist link
+playlist_link: StringVar = app.add_entry(400, 40)
+
+# Download button
+app.add_button("Download", 200, 40,
+               YTDownloader.start_download, playlist_link.get())
+
+# Run the app
+app.run_app()
