744e4bdc00155cbb112a5883d442f84d4a36fe49
Khair Alanam <anaambasheer@gmail.com>
2023-12-11 22:21:24 +0530

    Add methods for making entry boxes and labels

--- classes/Application.py
+++ classes/Application.py
@@ -1,10 +1,23 @@
 import customtkinter as ctk
+import tkinter as tk
 
 
 class Application:
     def __init__(self, name, dimensions):
-        app = ctk.CTk()
-        app.title(name)
-        app.geometry(dimensions)
-        app.mainloop()
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("green")
+        self.app = ctk.CTk()
+        self.app.title(name)
+        self.app.geometry(dimensions)
+        self.app.mainloop()
         print("Initialised!")
+
+    def add_label(self, name, x, y):
+        main_label = ctk.CTkLabel(self.app, text=name)
+        main_label.pack(padx=x, pady=y)
+
+    def add_entry(self, text, x, y):
+        entry_var = ctk.StringVar()
+        entry_box = ctk.CTkEntry(self.app, text=text, textvariable=entry_var)
+        entry_box.pack(padx=x, pady=y)
+        return entry_var
