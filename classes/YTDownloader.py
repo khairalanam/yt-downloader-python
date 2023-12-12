a885faaddaff1ebebcab7b4944700adbf467cb0a
Khair Alanam <anaambasheer@gmail.com>
2023-12-12 16:52:21 +0530

    Add method to download videos

--- classes/YTDownloader.py
+++ classes/YTDownloader.py
@@ -1,4 +1,7 @@
 from .Application import Application
+from multiprocessing import Pool
+from pytube import Playlist, YouTube
+from time import time
 
 
 class YTDownloader(Application):
@@ -7,5 +10,20 @@ class YTDownloader(Application):
         print("YT Initialised!")
 
     @staticmethod
-    def start_download(link):
-        print("Started Downloading!")
+    def start_playlist_download(playlist_link):
+        try:
+            start = time()
+            playlist_object = Playlist(playlist_link.get())
+            list_of_videos = [video.streams.get_highest_resolution()
+                              for video in playlist_object.videos]
+            with Pool() as pool:
+                pool.map(YTDownloader.download_video, list_of_videos)
+        except:
+            YTDownloader.show_error(
+                "Invalid Link", "The Youtube link provided is invalid!")
+        else:
+            print(f"{time() - start} s")
+
+    @staticmethod
+    def download_video(video):
+        video.download()
