e26e67830c85cbd3b9033de01ff1c83c3be8d513
Khair Alanam <anaambasheer@gmail.com>
2023-12-12 19:31:44 +0530

    modify download to show title and image

--- classes/YTDownloader.py
+++ classes/YTDownloader.py
@@ -9,18 +9,18 @@ class YTDownloader(Application):
         super().__init__(name, dimensions)
         print("YT Initialised!")
 
-    @staticmethod
-    def start_playlist_download(playlist_link):
+    def start_playlist_download(self, playlist_link):
         try:
             start = time()
             playlist_object = Playlist(playlist_link.get())
+            self.add_label(playlist_object.title, 40, 40)
             list_of_videos = [video.streams.get_highest_resolution()
                               for video in playlist_object.videos]
+            self.add_image([*playlist_object.videos][0].thumbnail_url, 40, 40)
             with Pool() as pool:
                 pool.map(YTDownloader.download_video, list_of_videos)
         except:
-            YTDownloader.show_error(
-                "Invalid Link", "The Youtube link provided is invalid!")
+            self.add_label("Invalid Link!", 40, 40)
         else:
             print(f"{time() - start} s")
 
